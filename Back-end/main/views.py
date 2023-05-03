994eeb6a8122fe669a8817e9be399d0751b47dd2
Cristian Marius <c84247578@china.huawei.com>
2023-05-03 17:18:30 +0300

    Changed to class based views.

--- Back-end/main/views.py
+++ Back-end/main/views.py
@@ -4,306 +4,72 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Teacher, Classroom, Subject, Student, ClassAssignment, Timetable, Grade
 from .serializers import TeacherSerializer, ClassroomSerializer, SubjectSerializer, StudentSerializer, ClassAssignmentSerializer, TimetableSerializer, GradeSerializer
-from rest_framework import generics, mixins
-from rest_framework.views import APIView
+from rest_framework import generics
+
 
 
 # Create your views here.
 
 
 # Define the API Methods supported.
-@api_view(['GET', 'POST'])
-def teachers(request):
-    if request.method == "GET":
-        # Get all teachers
-        teachers_list = Teacher.objects.all()
-        
-        # Serialize them
-        serializer_object = TeacherSerializer(teachers_list, many=True)
+class TeachersList(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()    # Get all teachers
+    serializer_class = TeacherSerializer    # Serialize them
 
-        # Return Json Response
-        return Response(serializer_object.data)
-    
-    elif request.method == "POST":
-        # Receive request data and save it.
-        serializer_object = TeacherSerializer(data=request.data)
-
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(serializer_object.data, status=status.HTTP_201_CREATED)
-        
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def teacher_info(request, id):
-
-    try:
-        # Get specific teacher info based on primary key id.
-        teacher_info = Teacher.objects.get(pk=id)
-    except Teacher.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == "GET":
-        # Serialize the object and return the data.
-        serializer_object = TeacherSerializer(teacher_info)
-        return Response(serializer_object.data)
-
-    elif request.method == "PUT":
-        serializer_object = TeacherSerializer(teacher_info, data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(serializer_object.data, status=status.HTTP_202_ACCEPTED)
-        return Response(serializer_object.errors, status=status.HTTP_404_NOT_FOUND)
-    
-    elif request.method == "DELETE":
-        teacher_info.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-
-class TeachersList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
-    # Get all teachers
-    queryset = Teacher.objects.all()
-    
-    # Serialize them
-    serializer_class = TeacherSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class TeacherDetails(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+class TeacherDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+class ClassroomsList(generics.ListCreateAPIView):
+    queryset = Classroom.objects.all()
+    serializer_class = ClassroomSerializer
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-
-@api_view(['GET', 'POST'])
-def classrooms(request):
-    if request.method == "GET":
-        classroom_list = Classroom.objects.all()
-        serializer_object = ClassroomSerializer(classroom_list, many=True)
-        return Response(serializer_object.data)
-    
-    elif request.method == "POST":
-        serializer_object = ClassroomSerializer(data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(serializer_object.data, status=status.HTTP_201_CREATED)
-        
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def classroom_info(request, id):
-    try:
-        classroom_info = Classroom.objects.get(pk=id)
-    except Classroom.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == "GET":
-        serializer_object = ClassroomSerializer(classroom_info)
-        return Response(serializer_object.data)
-    
-    elif request.method == "PUT":
-        serializer_object = ClassroomSerializer(classroom_info, data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(serializer_object.data, status=status.HTTP_202_ACCEPTED)
-        return Response(serializer_object.data, status=status.HTTP_404_NOT_FOUND)
-    
-    elif request.method == "DELETE":
-        classroom_info.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-
-@api_view(["GET", "POST"])
-def subject(request):
-    if request.method == "GET":
-        subject_list = Subject.objects.all()
-        serializer_object = SubjectSerializer(subject_list, many=True)
-        return Response(serializer_object.data)
-    
-    elif request.method == "POST":
-        serializer_object = SubjectSerializer(data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(serializer_object.data, status=status.HTTP_201_CREATED)
+class ClassroomDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Classroom.objects.all()
+    serializer_class = ClassroomSerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def subject_info(request, id):
-    try:
-        subject_info = Subject.objects.get(pk=id)
-    except Subject.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == "GET":
-        serializer_object = SubjectSerializer(subject_info)
-        return Response(serializer_object.data)
-    
-    elif request.method == "PUT":
-        serializer_object = SubjectSerializer(subject_info, data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(status=status.HTTP_202_ACCEPTED)
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    elif request.method == "DELETE":
-        subject_info.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
+class SubjectsList(generics.ListCreateAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
 
-@api_view(['GET', 'POST'])
-def student(request):
-    if request.method == "GET":
-        student_list = Student.objects.all()
-        serializer_object = StudentSerializer(student_list, many=True)
-        return Response(serializer_object.data)
-    
-    elif request.method == "POST":
-        serializer_object = StudentSerializer(data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(serializer_object.data, status=status.HTTP_201_CREATED)
-        
+class SubjectDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def student_info(request, id):
-    try:
-        student_info = Student.objects.get(pk=id)
-    except Student.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == "GET":
-        serializer_object = StudentSerializer(student_info)
-        return Response(serializer_object.data)
-    
-    elif request.method == "PUT":
-        serializer_object = StudentSerializer(student_info, data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(status=status.HTTP_202_ACCEPTED)
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    elif request.method == "DELETE":
-        student_info.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
 
-@api_view(['GET', 'POST'])
-def class_assignment(request):
-    if request.method == "GET":
-        class_assignment_list = ClassAssignment.objects.all()
-        serializer_object = ClassAssignmentSerializer(class_assignment_list, many=True)
-        return Response(serializer_object.data)
-    
-    elif request.method == "POST":
-        serializer_object = ClassAssignmentSerializer(data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(serializer_object.data, status=status.HTTP_201_CREATED)
-        
+class StudentsList(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def class_assignment_info(request, id):
-    try:
-        class_assignment_info = ClassAssignment.objects.get(pk=id)
-    except ClassAssignment.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == "GET":
-        serializer_object = ClassAssignmentSerializer(class_assignment_info)
-        return Response(serializer_object.data)
-    
-    elif request.method == "PUT":
-        serializer_object = ClassAssignmentSerializer(class_assignment_info, data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(status=status.HTTP_202_ACCEPTED)
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    elif request.method == "DELETE":
-        class_assignment_info.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
+class StudentDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-@api_view(['GET', 'POST'])
-def timetable(request):
-    if request.method == "GET":
-        timetable_list = Timetable.objects.all()
-        serializer_object = TimetableSerializer(timetable_list, many=True)
-        return Response(serializer_object.data)
-    
-    elif request.method == "POST":
-        serializer_object = TimetableSerializer(data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(serializer_object.data, status=status.HTTP_201_CREATED)
-        
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def timetable_info(request, id):
-    try:
-        timetable_info = Timetable.objects.get(pk=id)
-    except Timetable.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == "GET":
-        serializer_object = TimetableSerializer(timetable_info)
-        return Response(serializer_object.data)
-    
-    elif request.method == "PUT":
-        serializer_object = TimetableSerializer(timetable_info, data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(status=status.HTTP_202_ACCEPTED)
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    elif request.method == "DELETE":
-        timetable_info.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
+class ClassAssignmentsList(generics.ListCreateAPIView):
+    queryset = ClassAssignment.objects.all()
+    serializer_class = ClassAssignmentSerializer
 
-@api_view(['GET', 'POST'])
-def grade(request):
-    if request.method == "GET":
-        grade_list = Grade.objects.all()
-        serializer_object = GradeSerializer(grade_list, many=True)
-        return Response(serializer_object.data)
-    
-    elif request.method == "POST":
-        serializer_object = GradeSerializer(data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(serializer_object.data, status=status.HTTP_201_CREATED)
-        
+class ClassAssignmentDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ClassAssignment.objects.all()
+    serializer_class = ClassAssignmentSerializer
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def grade_info(request, id):
-    try:
-        grade_info = Grade.objects.get(pk=id)
-    except Grade.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == "GET":
-        serializer_object = GradeSerializer(grade_info)
-        return Response(serializer_object.data)
-    
-    elif request.method == "PUT":
-        serializer_object = GradeSerializer(grade_info, data=request.data)
-        if serializer_object.is_valid():
-            serializer_object.save()
-            return Response(status=status.HTTP_202_ACCEPTED)
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    elif request.method == "DELETE":
-        grade_info.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class TimetablesList(generics.ListCreateAPIView):
+    queryset = Timetable.objects.all()
+    serializer_class = TimetableSerializer
+
+class TimetableDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Timetable.objects.all()
+    serializer_class = TimetableSerializer
+
+
+class GradesList(generics.ListCreateAPIView):
+    queryset = Grade.objects.all()
+    serializer_class = GradeSerializer
+
+class GradeDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Grade.objects.all()
+    serializer_class = GradeSerializer
